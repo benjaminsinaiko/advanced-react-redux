9a7b31656d83303d3cb5ad1b434547274e295d8e
benjaminsinaiko <ben.jamin@outlook.com>
2020-07-22 10:22:12 -0500

    Add moxios test to fetch comments

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched 1' }, { name: 'Fetched 2' }],
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
   // Attempt to render the entire app
   const wrapper = mount(
     <Root>
@@ -12,5 +25,15 @@ it('can fetch a list of comments and display them', () => {
   );
 
   // Find the 'fetchComments' button and click it
+  wrapper.find('.fetch-comments').simulate('click');
+
   // Expect to find a list of comments!
+  setTimeout(() => {
+    wrapper.update();
+
+    expect(wrapper.find('li').length).toEqual(2);
+
+    done();
+    wrapper.unmount();
+  }, 100);
 });
