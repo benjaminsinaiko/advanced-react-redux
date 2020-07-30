57f18af752215afe09cc2ea204d419e9e4cf8fe6
benjaminsinaiko <ben.jamin@outlook.com>
2020-07-30 12:45:12 -0500

    Add signup with error handling and redirect

--- auth-client/src/components/auth/Signup.js
+++ auth-client/src/components/auth/Signup.js
@@ -6,7 +6,9 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   onSubmit = (formProps) => {
-    this.props.signup(formProps);
+    this.props.signup(formProps, () => {
+      this.props.history.push('/feature');
+    });
   };
 
   render() {
@@ -22,10 +24,15 @@ class Signup extends Component {
           <label>Password</label>
           <Field name='password' type='password' component='input' autoComplete='none' />
         </fieldset>
+        <div>{this.props.errorMessage}</div>
         <button>Sign Up!</button>
       </form>
     );
   }
 }
 
-export default compose(connect(null, actions), reduxForm({ form: 'signup' }))(Signup);
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(connect(mapStateToProps, actions), reduxForm({ form: 'signup' }))(Signup);
